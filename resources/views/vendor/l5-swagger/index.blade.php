a36e6e67b81ba632c5e935de3b0243937fcf11b3
os1 <os1@gmail.com>
2025-04-12 23:32:05 +0700

    cập nhật document api 1

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
-    <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}">
-    <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16"/>
+    <link rel="stylesheet" type="text/css" href="{{ preg_replace('/^http:/', 'https:', l5_swagger_asset($documentation, 'swagger-ui.css')) }}">
+    <link rel="icon" type="image/png" href="{{ preg_replace('/^http:/', 'https:', l5_swagger_asset($documentation, 'favicon-32x32.png')) }}" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="{{ preg_replace('/^http:/', 'https:', l5_swagger_asset($documentation, 'favicon-16x16.png')) }}" sizes="16x16"/>
     <style>
     html
     {
@@ -118,9 +118,8 @@
 
 <body @if(config('l5-swagger.defaults.ui.display.dark_mode')) id="dark-mode" @endif>
 <div id="swagger-ui"></div>
-
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"></script>
+<script src="{{ preg_replace('/^http:/', 'https:', l5_swagger_asset($documentation, 'swagger-ui-bundle.js')) }}"></script>
+<script src="{{ preg_replace('/^http:/', 'https:', l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js')) }}"></script>
 <script>
     window.onload = function() {
         // Build a system
